75afca3db14a5cd5965f3608e22041053c453811
denis <new.den84@gmail.com>
2019-09-23 20:12:42 +0300

    add DB model

--- controller/TableController.php
+++ controller/TableController.php
@@ -11,14 +11,7 @@ class TableController
         $page = new TableForm(); 
         $page->getHead('Выберите таблицу и поля');
         $db = new DB($dbpath);
-        $tables = $db->query("select * from sqlite_master where type = 'table'");
-        //$tableinfo = $db->query("PRAGMA table_info('".$tables->fetchArray()['tbl_name']."')");
-        //$sqltable = "PRAGMA table_info('".$tables->fetchArray()['name']."')";
-     //   $tableinfo = $db->query("select * from ".$tables->fetchArray()['name']);
-       // echo "$sqltable\n";
-       print_r($tables->fetchArray());
-       echo "\n!!!!!!!!!\n";
-        echo $tables->fetchArray()['sql'];
+        $db->getTablesInfo();
     }
 }
 
--- model/DB.php
+++ model/DB.php
@@ -9,4 +9,15 @@ class DB extends SQLite3
     {
         $this->open($dbpath);
     }
+    public function getTablesInfo()
+    {
+       $tables = $this->query("select * from sqlite_master where type = 'table'");
+       echo "test - ".$tables->fetchArray()['sql'];
+        
+    }
+    /*Метод принимает */
+    public function getInfoTableFromSQL($sql)
+    {
+        
+    }
 }
