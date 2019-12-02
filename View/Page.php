2627ccc283acb4cece3905fa0de93aa0478a87cc
denis <new.den84@gmail.com>
2019-12-02 21:05:52 +0200

    add styles and script link in SqliteForm

--- View/Page.php
+++ View/Page.php
@@ -12,11 +12,16 @@ class Page
     {
         echo "<html>\n<head>\n<title>$title</title>\n</head>\n";
         if (count($links > 0)) {
-            
+            array_map(function($link){
+                echo "<link href='$link' rel='stylesheet'>";
+            }, $links);   
         }
     }
-    public function getFoot()
+    public function getFoot($links)
     {
-        
+        array_map(function($link){
+            echo "<script src='$link'></script>\n";
+        }, $links);
+        echo "</html>";
     }
 }
